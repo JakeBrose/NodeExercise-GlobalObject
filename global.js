a54530dd32535d2d9b53da4fac9b7486149a49fe
JakeBrose <mordecaistudios@gmail.com>
2018-03-05 12:43:49 -0600

    currently implementing destructering, not fully func

--- global.js
+++ global.js
@@ -5,4 +5,10 @@ process.env.versionV8 = "5.0.71.57";
 
 console.log(`Node.js version: ${process.env.versionNode}\nV8 version ${process.env.versionV8}`);
 
+// // // // // //
+// V2 //
+// // // // // // 
 
+process.env.versionObj = ({ a, b } = { a: "v6 .3 .1", b: "5.0.71.57" });
+
+console.log(process.env.versionObj.a);
